2f36cab13fe9dd5363796c589e6dd629eaa4c770
厉增伟 <lee@izhuoyi.com>
2020-10-18 10:32:13 +0800

    update

--- routes.php
+++ routes.php
@@ -1,3 +1,7 @@
 <?php
 
+// H5 高级 API 鉴权
+Route::post('dingding/jsapi-signature', [\Smartymoon\DingTalk\Controllers\DingDingController::class, 'jsapiSignature']);
+
+// 钉钉事件回调
 \Route::post('dingding/event/callback', [\Smartymoon\DingTalk\Controllers\DingDingController::class, 'serve']);
--- src/Controllers/DingDingController.php
+++ src/Controllers/DingDingController.php
@@ -4,6 +4,7 @@ namespace Smartymoon\DingTalk\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Smartymoon\DingTalk\Api\DingApi;
 use Smartymoon\DingTalk\Helpers\Encryptor;
 
 class DingDingController extends \App\Http\Controllers\Controller
@@ -13,6 +14,14 @@ class DingDingController extends \App\Http\Controllers\Controller
     public function __construct()
     {
        $this->encryptor = new Encryptor(config('ding.corp_id'), config('ding.token'), config('ding.aes_key'));
+    }
+
+    // 一组 api 鉴权参数，用于帮前端调用高级的 api
+    public function jsapiSignature(Request $request)
+    {
+        $params = DingApi::agent($request->input('agent'))
+            ->h5app->getSignature($request->input('url'));
+        return $params;
     }
 
     /**
@@ -27,6 +36,7 @@ class DingDingController extends \App\Http\Controllers\Controller
          * 处理回调事件，这里是具体业务
          */
         $payload = $this->getPayload($request);
+        \Log::channel('ding')->info('有钉钉回调事件来了', $payload);
         $eventType = $payload['EventType'];
         if (isset(config('ding.events')[$eventType])) {
             $events = config('ding.events');
